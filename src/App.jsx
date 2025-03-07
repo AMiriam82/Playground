082e0f82715c79062fd2204399b5c22de00832d1
AMiriam82 <miriam.allinger@nextfarming.de>
2025-03-07 05:58:05 +0000

    Intermediate 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import "./App.css";
+import chef from "./images/Chefkoch.jpg";
 
 function Header(props)  {
     return (
@@ -13,16 +14,32 @@ function Header(props)  {
 const items = [
     "Macaroni and Cheese",
     "Salmon with Potatoes",
-    "Tofu with Vegetables"
+    "Tofu with Vegetables",
+    "Minestrone Soup"
 ]
+
+const dishObjects = items.map((dish, i) => ({
+    id: i,
+    title: dish
+}))
+
+console.log(dishObjects);
 
 function Main({dishes}) {
     return (
+    <>
+        <div>
+            <h2>Welcome to this beautiful restaurant!</h2>
+        </div>
+        <main>
+            <img src={chef} height={200} alt="A photo of a smilin chef owner" />
         <ul>
             {dishes.map((dish) => (
-                <li style = {{listStyleType: "none"}}>{dish}</li>
+                <li key={dish.id} style = {{listStyleType: "none"}}>{dish.title}</li>
             ))}
         </ul>
+        </main>
+    </>
     )
 }
 
@@ -30,7 +47,7 @@ function App() {
     return (
         <div>
             <Header name="Alex" year={new Date().getFullYear()} />            
-            <Main dishes={items}/>
+            <Main dishes={dishObjects}/>
         </div>
     )
 }
